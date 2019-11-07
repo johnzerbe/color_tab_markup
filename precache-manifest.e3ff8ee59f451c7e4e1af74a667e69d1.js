d7ae6ff81ea0ac50ab8061d53637c9b64e95a4f5
John Z <jczerbe14@icloud.com>
2019-11-07 14:19:29 -0700

    Updates

---
+++ precache-manifest.e3ff8ee59f451c7e4e1af74a667e69d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ecb6cc9b538f0e3b301d0d4a1f74a4",
+    "url": "/color_tab_markup/index.html"
+  },
+  {
+    "revision": "5d834caa6f1c1d7b416d",
+    "url": "/color_tab_markup/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ba8d398cba3150de99b4",
+    "url": "/color_tab_markup/static/js/2.4db0ead7.chunk.js"
+  },
+  {
+    "revision": "5d834caa6f1c1d7b416d",
+    "url": "/color_tab_markup/static/js/main.00ff4208.chunk.js"
+  },
+  {
+    "revision": "63573592182644c8e5a7",
+    "url": "/color_tab_markup/static/js/runtime-main.a9c9e3b0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/color_tab_markup/static/media/logo.25bf045c.svg"
+  }
+]);
